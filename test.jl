533dfcd5e640e9be6a61c201ead8ce21480ad13b
zhangvwk95 <zhangvwk@stanford.edu>
2018-11-13 15:19:19 -0800

    test

---
+++ test.jl
@@ -0,0 +1,135 @@
+# activate project environment
+# include these lines of code in any future scripts/notebooks
+#---
+import Pkg
+if !haskey(Pkg.installed(), "AA228FinalProject")
+    jenv = joinpath(dirname(@__FILE__()), ".") # this assumes the notebook is in the same dir
+    # as the Project.toml file, which should be in top level dir of the project. 
+    # Change accordingly if this is not the case.
+    Pkg.activate(jenv)
+end
+#---
+
+using AA228FinalProject
+using POMDPs
+using POMDPPolicies
+using BeliefUpdaters
+using ParticleFilters
+using POMDPSimulators
+using Cairo
+using Gtk
+using Random
+using Printf
+
+
+""" Define sensor and construct POMDP """
+
+sensor = Lidar() # or Bumper() for the bumper version of the environment
+config = 3 # 1,2, or 3
+m = RoombaPOMDP(sensor=sensor, mdp=RoombaMDP(config=config));
+
+""" Setting up a Particle Filter """
+
+num_particles = 2000
+resampler = LidarResampler(num_particles, LowVarianceResampler(num_particles))
+# for the bumper environment
+# resampler = BumperResampler(num_particles)
+
+spf = SimpleParticleFilter(m, resampler)
+
+v_noise_coefficient = 2.0
+om_noise_coefficient = 0.5
+
+belief_updater = RoombaParticleFilter(spf, v_noise_coefficient, om_noise_coefficient);
+
+""" Define a policy """
+
+# Define the policy to test
+mutable struct ToEnd <: Policy
+    ts::Int64 # to track the current time-step.
+end
+
+# extract goal for heuristic controller
+goal_xy = get_goal_xy(m)
+
+# define a new function that takes in the policy struct and current belief,
+# and returns the desired action
+function POMDPs.action(p::ToEnd, b::ParticleCollection{RoombaState})
+    
+    # spin around to localize for the first 25 time-steps
+    if p.ts < 25
+        p.ts += 1
+        return RoombaAct(0.,1.0) # all actions are of type RoombaAct(v,om)
+    end
+    p.ts += 1
+    
+    # after 25 time-steps, we follow a proportional controller to navigate
+    # directly to the goal, using the mean belief state
+    
+    # compute mean belief of a subset of particles
+    s = mean(b)
+    
+    # compute the difference between our current heading and one that would
+    # point to the goal
+    goal_x, goal_y = goal_xy
+    x,y,th = s[1:3]
+    ang_to_goal = atan(goal_y - y, goal_x - x)
+    del_angle = wrap_to_pi(ang_to_goal - th)
+    
+    # apply proportional control to compute the turn-rate
+    Kprop = 1.0
+    om = Kprop * del_angle
+    
+    # always travel at some fixed velocity
+    v = 5.0
+    
+    return RoombaAct(v, om)
+end
+
+""" Simulation and rendering """
+
+# first seed the environment
+Random.seed!(2)
+
+# reset the policy
+p = ToEnd(0) # here, the argument sets the time-steps elapsed to 0
+
+# run the simulation
+c = @GtkCanvas()
+win = GtkWindow(c, "Roomba Environment", 600, 600)
+for (t, step) in enumerate(stepthrough(m, p, belief_updater, max_steps=100))
+    @guarded draw(c) do widget
+        
+        # the following lines render the room, the particles, and the roomba
+        ctx = getgc(c)
+        set_source_rgb(ctx,1,1,1)
+        paint(ctx)
+        render(ctx, m, step)
+        
+        # render some information that can help with debugging
+        # here, we render the time-step, the state, and the observation
+        move_to(ctx,300,400)
+        show_text(ctx, @sprintf("t=%d, state=%s, o=%.3f",t,string(step.s),step.o))
+    end
+    show(c)
+    sleep(0.1) # to slow down the simulation
+end
+
+""" Evaluation """
+
+using Statistics
+
+total_rewards = []
+
+for exp = 1:5
+    println(string(exp))
+    
+    Random.seed!(exp)
+    
+    p = ToEnd(0)
+    traj_rewards = sum([step.r for step in stepthrough(m,p,belief_updater, max_steps=100)])
+    
+    push!(total_rewards, traj_rewards)
+end
+
+@printf("Mean Total Reward: %.3f, StdErr Total Reward: %.3f", mean(total_rewards), std(total_rewards)/sqrt(5))
